c88d9cc2f25e29924b3916946c2e643069bb31fb
Antoine Catt <antoinecatt@gmail.com>
2018-10-01 14:35:16 -0700

    added factorial solution

--- recursion.js
+++ recursion.js
@@ -33,13 +33,85 @@
 // I would then take a character out of the string and save it then return the remaining characters in the string
 // until the string is empty. Then return it as whole in reverse
 
-let reverseStr = str => {
-  if (str === "") {
-    return "";
+// let reverseStr = str => {
+//   if (str === "") {
+//     return "";
+//   }
+
+//   let char = str[str.length - 1];
+//   return char + reverseStr(str.slice(0, str.length - 1));
+// };
+
+// console.log(reverseStr("Antoine"));
+
+// nth Triangular Number
+// an input would be a number and the output that i want will the number of stars
+// The number would then have to multiply itself by 2 to produce the next number of stars
+//
+
+// const triangle = num => {
+//   if (num === 0) {
+//     return;
+//   }
+
+//   let star = "*";
+//   return "*" + triangle(num - 1);
+// };
+
+// console.log(triangle(5));
+
+// String Splitter
+
+// const strSplitter = (str, index) => {
+//   // base case
+//   if (str === "") {
+//     return "";
+//   }
+
+//   let char = str.slice(index, str.length);
+//   return strSplitter
+// };
+
+// var str = "The quick brown fox jumped over the lazy dog.";
+
+// console.log(strSplitter(str, 10));
+
+// Factorial
+
+// const factorial = n => {
+// let f = [];
+// if (n === 0 || n === 1) {
+//   return 1;
+// }
+
+// if (f[n] > 0) {
+//   return f[n];
+// }
+// return (f[n] = factorial(n - 1) * n);
+// };
+
+// console.log(factorial(10));
+
+// function binaryRep(num) {
+//   //base case
+//   if (num <= 0) {
+//     return "";
+//   }
+//   //recursive case
+//   const binary = Math.floor(num % 2);
+//   return binaryRep(Math.floor(num / 2)) + binary;
+// }
+
+// console.log(binaryRep(25));
+
+function factorialize(num) {
+  if (num < 0) {
+    return undefined;
   }
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+}
 
-  let char = str[str.length - 1];
-  return char + reverseStr(str.slice(0, str.length - 1));
-};
-
-console.log(reverseStr("Antoine"));
+console.log(factorialize(5));
